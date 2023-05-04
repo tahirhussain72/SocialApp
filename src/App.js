e3f6a1a79e25b870df9fe01f64d377b96842b0ad
Tahir <tahir.toorie72@gmail.com>
2023-05-04 12:25:49 +0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home/Home';
-import Profile from './pages/profile/Profile';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
+
 
 
 function App() {
